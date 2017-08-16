a73622e46f144c1f967158aebdeea67b700521f4
Johnathan Gilday <johnathan.gilday@contrastsecurity.com>
2017-08-16 09:09:32 -0400

    iterm shell integration

--- install.sh
+++ install.sh
@@ -84,5 +84,10 @@ curl -o "$HOME/bin/imgcat" https://raw.githubusercontent.com/gnachman/iTerm2/mas
 curl -o "$HOME/bin/imgls" https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgls
 chmod ug+rx "$HOME/bin/imgcat" "$HOME/bin/imgls"
 
+if $osx; then
+    echo 'install iTerm shell integration'
+    curl -L https://iterm2.com/misc/`basename $SHELL`_startup.in -o ~/.iterm2_shell_integration.`basename $SHELL`
+fi
+
 #$HOME/dotfiles/install-bundles.sh
 
