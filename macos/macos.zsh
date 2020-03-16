110538d7310cb01a5e7765c93172b4e13e2270b0
Johnathan Gilday <johnathan.gilday@contrastsecurity.com>
2020-03-16 13:51:50 -0400

    :wrench: Homebrew auto-update timeout increase

--- macos/macos.zsh
+++ macos/macos.zsh
@@ -1,3 +1,6 @@
 path+=/Applications/kdiff3.app/Contents/MacOS
 path+=/usr/local/opt/coreutils/libexec/gnubin
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+# Only check for Homebrew updates once every 5 days
+export HOMEBREW_AUTO_UPDATE_SECS=432000
