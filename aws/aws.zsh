fb09fb94c2ffb335bf80842d4912a3b7c7c07147
Johnathan Gilday <johnathan.gilday@contrastsecurity.com>
2021-04-07 12:00:52 -0400

    :sparkles: AWS SSM Tunnel function

--- aws/aws.zsh
+++ aws/aws.zsh
@@ -1,3 +1,9 @@
 ssm() {
   aws ssm start-session --target $1
 }
+
+ssm-tunnel() {
+  aws ssm start-session --target $1 \
+    --document-name AWS-StartPortForwardingSession \
+    --parameters "{\"portNumber\":[\"$2\"],\"localPortNumber\":[\"$2\"]}" &> /dev/null &
+}
