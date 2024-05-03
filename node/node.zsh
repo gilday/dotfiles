a1fc538bfabf61b7d08553a1ac53bfdad2fa7179
Johnathan Gilday <me@johnathangilday.com>
2024-05-03 16:09:54 -0400

    📌 Node 20 LTS

--- node/node.zsh
+++ node/node.zsh
@@ -1,5 +1,5 @@
 if [[ "$OSTYPE" == darwin* ]]; then
   local prefix=$(brew --prefix)
   # prepend local node to path so it overrides system node
-  path=("${prefix}/opt/node@12/bin" $path)
+  path=("${prefix}/opt/node/bin" $path)
 fi
